274e0598c06fa7998645517e71a0c63c9dd5dde8
VictorSalasProgram <victorsalasok@gmail.com>
2023-05-09 00:06:03 -0300

    se agrego js

---
+++ js.js
@@ -0,0 +1,15 @@
+
+function mostrarAlerta(){
+    alert("Comprar tickets pronto");
+};
+function alertaOrador(){
+    alert("Pronto podras ser orador");
+};
+function alertaFormulario(){
+    alert("Formulario enviado, muchas gracias: " + document.getElementById("exampleFormControlInput2").value)
+}
+
+document.getElementById("botoncomprar").onclick = mostrarAlerta ;
+document.getElementById("botoncomprar2").onclick = mostrarAlerta ;
+document.getElementById("botonorador").onclick = alertaOrador ;
+document.getElementById("botondeenviar").onclick = alertaFormulario ;
