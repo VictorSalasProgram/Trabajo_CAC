e65a8203b210b3991763e687d7fb97610893d6ab
VictorSalasProgram <victorsalasok@gmail.com>
2023-05-17 13:39:14 -0300

    agregando pagina comprar tickets

--- js.js
+++ js.js
@@ -9,7 +9,7 @@ function alertaFormulario(){
     alert("Formulario enviado, muchas gracias: " + document.getElementById("exampleFormControlInput2").value)
 }
 
-document.getElementById("botoncomprar").onclick = mostrarAlerta ;
+
 document.getElementById("botoncomprar2").onclick = mostrarAlerta ;
 document.getElementById("botonorador").onclick = alertaOrador ;
 document.getElementById("botondeenviar").onclick = alertaFormulario ;
